e59ac002a07c979f89841ac351802c0e359cef88
zatkiller <gheedylan@gmail.com>
2021-11-29 01:26:56 +0800

    Add 5.4

--- assignment3/4/geometry.h
+++ assignment3/4/geometry.h
@@ -2,7 +2,11 @@ class Point {
     int x, y;
 
 public:
-    Point(int x = 0, int y = 0) { this->x = x;this->y = y; }
+    Point(int x=0, int y=0) { this->x = x; this->y = y; }
+    int getX() const { return this->x; }
+    int getY() const { return this->y; }
+    void setX(const int x) { this->x = x; }
+    void setY(const int y) { this->y = y; }
 };
 
 class PointArray {
